fcb2827b2ff14e7f0574d80f4eeb6eea5375f555
Kirill-MIPT <56344150+Kirill-MIPT@users.noreply.github.com>
2022-02-06 21:02:44 +0300

    создан класс ошибок в SystemObject. Исправлены Sys.exit в модуле Metadata
    создан класс ошибок в SystemObject. Исправлены Sys.exit в модуле Metadata

--- back-end/SystemObjects.py
+++ back-end/SystemObjects.py
@@ -2,7 +2,44 @@
 Системные объекты (Константы, ошибки и т.д.)
 """
 
-import sys
-import Constants
+class Error(Exception):
+    """
+      Errors
+    """
+
+    def __init__(self, p_error_text: str, p_module: str, p_class: str, p_def: str):
+        """
+
+        @param p_error_text: text of error
+        @param p_module: module, where error occured
+        @param p_class: class, where error occured (if class exists)
+        @param p_def: function, where error occured
+        """
+        self._error_text=p_error_text
+        self._module=p_module
+        self._class=p_class
+        self._def=p_def
+
+
+    @property
+    def error_text(self):
+        """
+        Text of errors
+        """
+        # checks
+        if self._error_text.__len__()==0:
+            print("Text is empty")
+        return self._error_text
+#разобраться с хешированием
+    # @property
+    # def error_code(self):
+    #     return hashlib.md5(self._module+"."+self._class+"."+self._def)
+
+    def raise_error(self):
+        """
+        Raises error message, that have been given in p_error_text param
+        """
+        self.args=[self.error_text]
+        raise self
 
 
