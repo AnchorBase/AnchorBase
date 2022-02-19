98fea74b772b3961ddcb06298a4ac0194a99f949
pdkhamrin <pdkhamrin@gmail.com>
2022-02-19 20:17:24 +0300

    Добавление файла с тест кейсами

---
+++ AnchorBase_json_тестирование.sql
@@ -0,0 +1,511 @@
+# 2.1.1
+{
+    "entity":"client",
+    "description":"client entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"key of the passenger",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"PostgreSQL: public, MSSQL: dbo, MySQL: public", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "table":"passenger",
+                    "column":"id"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"name",
+            "description":"name of the passenger",
+            "pk":0,
+            "datatype":"varchar",
+            "length":20,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"PostgreSQL: public, MSSQL: dbo, MySQL: public", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "table":"passenger",
+                    "column":"name"
+                }
+            ]
+        }
+    ]
+
+}
+
+# 2.1.2
+{
+    "entity":"client",
+    "description":"client entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"key of the passenger",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"name",
+            "description":"name of the passenger",
+            "pk":0,
+            "datatype":"varchar",
+            "length":20,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+            ]
+        }
+    ]
+
+}
+
+# 2.1.3
+{
+    "entity":"client",
+    "description":"client entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"key of the passenger",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"name",
+            "description":"name of the passenger",
+            "pk":1,
+            "datatype":"varchar",
+            "length":20,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+            ]
+        }
+    ]
+}
+
+# 2.1.4
+{
+    "entity":"client",
+    "description":"client entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"key of the passenger",
+            "pk":1,
+            "datatype":"varchar",
+            "length":100,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+            ]
+        }
+    ]
+
+}
+
+# 2.1.5
+{
+    "entity":"client",
+    "description":"client entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"key of the passenger",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"name",
+            "description":"name of the passenger",
+            "pk":0,
+            "datatype":"varchar",
+            "length":20,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"name"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"name"
+                }
+            ]
+        }
+    ]
+}
+
+{
+    "entity":"trip",
+    "description":"trip entity",
+    "attribute":
+    [
+        {
+            "name":"id",
+            "description":"trip number",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"trip_no"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"Trip",
+                    "column":"trip_no"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"trip_no"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"plane_nm",
+            "description":"name of the plane",
+            "pk":0,
+            "datatype":"varchar",
+            "length":20,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"plane"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"Trip",
+                    "column":"plane"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"plane"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"arrive_dttm",
+            "description":"arrive datetime",
+            "pk":0,
+            "datatype":"timestamp",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"time_in"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"Trip",
+                    "column":"time_in"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Trip",
+                    "column":"time_in"
+                }
+            ]
+        }
+    ]
+
+}
+
+
+# 2.1.5
+{
+    "entity":"client_trip",
+    "description":"link between client and trip",
+    "attribute":
+    [
+        {
+            "name":"trip_id",
+            "description":"trip number",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":"id of trip entity", # ИСПРАВИТЬ НА НУЖНОЕ!
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Pass_in_trip",
+                    "column":"trip_no"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"Pass_in_trip",
+                    "column":"trip_no"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Pass_in_trip",
+                    "column":"trip_no"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"client_id",
+            "description":"key of the client",
+            "pk":1,
+            "datatype":"int",
+            "length":null,
+            "link_entity":"id of client entity", # ИСПРАВИТЬ НА НУЖНОЕ!
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"passenger",
+                    "column":"id"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"passenger",
+                    "column":"id"
+                }
+            ]
+        }
+        ,
+        {
+            "name":"arrive_date",
+            "description":"arrive date",
+            "pk":0,
+            "datatype":"date",
+            "length":null,
+            "link_entity":null,
+            "source":
+            [
+                {
+                    "source":"id of PostgreSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Pass_in_trip",
+                    "column":"date"
+                }
+                ,
+                {
+                    "source":"id of MSSQL source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"dbo",
+                    "table":"Pass_in_trip",
+                    "column":"date"
+                }
+                ,
+                {
+                    "source":"id of source", # ИСПРАВИТЬ НА НУЖНОЕ!
+                    "schema":"public",
+                    "table":"Pass_in_trip",
+                    "column":"date"
+                }
+            ]
+        }
+    ]
+}
